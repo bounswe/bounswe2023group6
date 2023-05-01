3d221aeff670cb112235e808623ea8b1ca0eb7d0
Erkam Kavak <erkam.kavak@boun.edu.tr>
2023-05-02 00:54:53 +0300

    -Get game information endpoint added -Html pages for game information added and index.html changed

--- practice-app/app/views.py
+++ practice-app/app/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 import requests
+import json
 
 app = Flask(__name__)   # name is global variable returning program name which is app
 
@@ -59,4 +60,41 @@ def WorldTime():
     else:
         return render_template("worldtime.html")
 
+def get_game_id_from_name(game_name):
+    steam_api_game_list_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+    response = requests.get(steam_api_game_list_url).json()
+    all_games_list = response["applist"]["apps"]
+    game_name = "".join(game_name.lower().split())
+    for game in all_games_list:
+        if game_name == "".join(game["name"].lower().split()): 
+            return game["appid"]
+    return -1
 
+def get_game_information_from_id(game_id): 
+    steam_api_get_game_url = "https://store.steampowered.com/api/appdetails?appids={game_id}"
+    response = requests.get(steam_api_get_game_url.format(game_id=game_id)).json()
+    game_information = response[str(game_id)]["data"]
+    return game_information
+
+def get_game_information_from_name(game_name): 
+    game_id = get_game_id_from_name(game_name)
+    if game_id == -1: 
+        return None
+    game_information = get_game_information_from_id(game_id)
+    return game_information
+
+@app.route("/get_game_information", methods= ["GET", "POST"])
+def get_game_information():
+    error = None
+    if request.method == "POST":
+        game_name = request.form.get("game_name")
+        if not game_name:
+            error = 'Game name is required!'
+        else:
+            game_information = get_game_information_from_name(game_name)
+            if game_information == None: 
+                error = "Game is not found!"
+            else: 
+                # print(json.dumps(game_information, indent=4))
+                return render_template('show_game_information.html', response=game_information) 
+    return render_template("get_game_information.html", error=error)
