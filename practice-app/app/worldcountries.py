34e0709ac1f671e8d2b593253ba2162d7551b861
emresin <Emresin75@outlook.com>
2023-05-12 20:00:45 +0300

    Error handling is added

--- practice-app/app/worldcountries.py
+++ practice-app/app/worldcountries.py
@@ -29,36 +29,41 @@ def GetWorldCountries():
 @app.post("/worldcountries")
 def PostWorldCountries():
     data = request.form["country"]
-    
+    error = ""
 
 
     response = requests.get("https://restcountries.com/v3.1/name/" + data)
     if response.status_code == 404:
-        render_template()
-    parsed_data = response.json()
-    name = parsed_data[0]["name"]["common"]
-    official_name = parsed_data[0]["name"]["official"]
-    currencies_dict = parsed_data[0]["currencies"]
-    currencies = list()
+        error="The country that you are looking for couldn't found"
+       
+    elif response.status_code == 500:
+        error = "the server is not available at that moment try again later"
+    else:
+        parsed_data = response.json()
+        name = parsed_data[0]["name"]["common"]
+        official_name = parsed_data[0]["name"]["official"]
+        currencies_dict = parsed_data[0]["currencies"]
+        currencies = list()
 
-    for k, v in currencies_dict.items():
-        currencies.append(v["name"])
+        for k, v in currencies_dict.items():
+            currencies.append(v["name"])
 
-    cur_string = " ".join(currencies)
-    capital = parsed_data[0]["capital"][0]
-    region = parsed_data[0]["region"]
-    population = parsed_data[0]["population"]
+        cur_string = " ".join(currencies)
+        capital = parsed_data[0]["capital"][0]
+        region = parsed_data[0]["region"]
+        population = parsed_data[0]["population"]
 
-    CountryEntry = Country(
-        name=name,
-        official_name=official_name,
-        currency=cur_string,
-        capital=capital,
-        region=region,
-        population=population,
-    )
+        CountryEntry = Country(
+            name=name,
+            official_name=official_name,
+            currency=cur_string,
+            capital=capital,
+            region=region,
+            population=population,
+        )
 
-    session.add(CountryEntry)
-    session.commit()
-
-    return redirect(url_for("GetWorldCountries"))
+        session.add(CountryEntry)
+        session.commit()
+    
+    ret = session.query(Country).all()
+    return render_template("worldcountries.html",entries= ret,error= error)
