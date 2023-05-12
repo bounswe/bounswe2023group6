d5ac2dcb651b24d3c45ee00e4550fed1c1fbff6d
omertalib42 <omertalib42@gmail.com>
2023-05-12 12:44:05 +0300

    Apply code style w/ black

--- practice-app/app/tcorp.py
+++ practice-app/app/tcorp.py
@@ -22,6 +22,7 @@ def get_all_incidents(header):
     ).json()
     return all_incidents
 
+
 @app.route("/change_status/<new_status>", methods=["POST"])
 def change_status(new_status):
     instatus_api_key = os.environ.get("INSTATUS_API_KEY")
@@ -34,15 +35,16 @@ def change_status(new_status):
     elif new_status.lower() == "investigating":
         template_id = NO_TEMPLATE
     else:
-        return jsonify({'status': 404, 'message': 'Page not found'})
+        return jsonify({"status": 404, "message": "Page not found"})
 
     curr_inc = incidents[0]
     curr_inc_id = curr_inc["id"]
     curr_inc_status = curr_inc["status"]
 
     if new_status.lower() == curr_inc_status.lower():
-        return jsonify({'status': 200, 'message': 'Status already set to ' + curr_inc_status})
-
+        return jsonify(
+            {"status": 200, "message": "Status already set to " + curr_inc_status}
+        )
 
     update_incident = requests.post(
         f"{INSTATUS_URL}/v2/{PAGE_ID}/incidents/{curr_inc_id}/incident-updates/{template_id}",
@@ -50,9 +52,15 @@ def change_status(new_status):
     )
     if update_incident.status_code != 200:
         # returning 200 to show error of the external api successfully on the front end
-        return jsonify({'status': 200, 'message': f'Something went wrong while updating the status: {update_incident.text}'})
+        return jsonify(
+            {
+                "status": 200,
+                "message": f"Something went wrong while updating the status: {update_incident.text}",
+            }
+        )
 
-    return jsonify({'status': 200, 'message': 'Status changed to ' + new_status})
+    return jsonify({"status": 200, "message": "Status changed to " + new_status})
+
 
 @app.route("/get_current_status", methods=["GET"])
 def get_current_status():
@@ -62,10 +70,11 @@ def get_current_status():
     incidents = get_all_incidents(header)[0]
     curr_status = incidents["status"]
 
-    return jsonify({'status': 200, 'message': curr_status})
+    return jsonify({"status": 200, "message": curr_status})
+
 
 @app.route("/status_page")
 def get_status_page():
     curr_status_response = get_current_status()
     curr_status_dict = json.loads(curr_status_response.response[0])
-    return render_template("status_page.html", curr_status=curr_status_dict['message'])
+    return render_template("status_page.html", curr_status=curr_status_dict["message"])
