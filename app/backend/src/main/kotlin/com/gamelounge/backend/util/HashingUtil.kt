48d665801e2486747b075ceaf56fa12ee3e3f05d
Legervad <emre.turker@boun.edu.tr>
2023-10-22 11:57:09 +0300

    #275 implement hashing mechanism

---
+++ app/backend/src/main/kotlin/com/gamelounge/backend/util/HashingUtil.kt
@@ -0,0 +1,34 @@
+package com.gamelounge.backend.util
+
+import com.gamelounge.backend.constant.HashingConstants
+import java.security.SecureRandom
+import java.security.spec.KeySpec
+import java.util.*
+import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+object HashingUtil {
+    private fun generateRandomSalt(): ByteArray {
+        val random = SecureRandom()
+        val salt = ByteArray(16)
+        random.nextBytes(salt)
+        return salt
+    }
+
+    private fun ByteArray.toHexString(): String =
+        HexFormat.of().formatHex(this)
+
+
+    fun generateHash(password: String): Pair<ByteArray, ByteArray> {
+        val salt = generateRandomSalt()
+
+        val combinedSalt = "$salt${HashingConstants.SECRET}".toByteArray()
+        val factory: SecretKeyFactory = SecretKeyFactory.getInstance(HashingConstants.ALGORITHM)
+        val spec: KeySpec = PBEKeySpec(password.toCharArray(), combinedSalt, HashingConstants.ITERATIONS, HashingConstants.KEY_LENGTH)
+        val key: SecretKey = factory.generateSecret(spec)
+        val hash: ByteArray = key.encoded
+
+        return Pair(hash, salt)
+    }
+}
