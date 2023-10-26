dfcb446663d37a38501e8de0762384f6edc66db7
Emre <74266381+Legervad@users.noreply.github.com>
2023-10-26 21:51:18 +0300

    Backend/enhancement/implement swagger #289 (#296)
    * #289 implement swagger
    
    * #289 minor changes

--- app/backend/build.gradle.kts
+++ app/backend/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.5")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
