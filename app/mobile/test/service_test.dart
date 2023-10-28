89b9fb0dab26ad081edf4cc3f29be136f0a0cad8
Erkam Kavak <erkam.kavak@boun.edu.tr>
2023-10-28 22:02:39 +0300

    - tests added for baseService class - login model corrected based on backend endpoints - error catching added to baseService class

---
+++ app/mobile/test/service_test.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:mobile/constants/network_constants.dart';
+import 'package:mobile/data/models/dto/login/login_request.dart';
+import 'package:mobile/data/models/dto/login/login_response.dart';
+import 'package:mobile/data/models/login_model.dart';
+import 'package:mobile/data/models/service_response.dart';
+import 'package:test/test.dart';
+
+import "package:mobile/data/services/base_service.dart";
+
+void main() {
+  test("Service test", () async {
+    final dio = Dio();
+    final response = await dio.post(
+      '${NetworkConstants.BASE_LOCAL_URL}/register',
+      data: {
+        'username': 'erkam.kavak',
+        'password': '123456',
+        'email': 'erkam@boun.edu.tr',
+        'name': 'Erkam',
+        'surname': 'Kavak',
+        'image': await File.fromUri(Uri.parse('./lib/assets/avatar.jpg'))
+            .readAsBytes(),
+      },
+    );
+    expect(response.statusCode, 200);
+    expect(response.data['message'], 'Registered successfully!');
+  });
+
+  test("Login test", () async {
+    final BaseNetworkService service = BaseNetworkService();
+    expect(service, isNotNull);
+
+    const String path = '/login';
+    final LoginModel loginModel = LoginModel();
+    loginModel.username = 'erkam.kavak';
+    loginModel.password = '123456';
+    ServiceResponse serviceResponse = await service
+        .sendRequestSafe<LoginDTORequest, LoginDTOResponse, LoginModel>(
+            path, loginModel, 'POST');
+
+    expect(serviceResponse, isNotNull);
+    if (serviceResponse.success) {
+      expect(loginModel.message, isNotNull);
+      expect(loginModel.message, 'Logged in successfully.');
+      // expect(loginModel.sessionId, isNotNull);
+    } else {
+      expect(serviceResponse.errorMessage, isNotNull);
+      print(serviceResponse.errorMessage);
+    }
+  });
+}
