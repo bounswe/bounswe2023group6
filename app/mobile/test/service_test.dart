5e529ee802fda44be7ae924cbd093292f4fcc509
Erkam Kavak <erkam.kavak@boun.edu.tr>
2023-10-29 14:00:01 +0300

    exception catch added to raw service test

--- app/mobile/test/service_test.dart
+++ app/mobile/test/service_test.dart
@@ -13,20 +13,26 @@ import "package:mobile/data/services/base_service.dart";
 void main() {
   test("Service test", () async {
     final dio = Dio();
-    final response = await dio.post(
-      '${NetworkConstants.BASE_LOCAL_URL}/register',
-      data: {
-        'username': 'erkam.kavak',
-        'password': '123456',
-        'email': 'erkam@boun.edu.tr',
-        'name': 'Erkam',
-        'surname': 'Kavak',
-        'image': await File.fromUri(Uri.parse('./lib/assets/avatar.jpg'))
-            .readAsBytes(),
-      },
-    );
-    expect(response.statusCode, 200);
-    expect(response.data['message'], 'Registered successfully!');
+
+    try {
+      final response = await dio.post(
+        '${NetworkConstants.BASE_LOCAL_URL}/register',
+        data: {
+          'username': 'erkam.kavakk',
+          'password': '123456',
+          'email': 'erkam@boun.edu.tr',
+          'name': 'Erkam',
+          'surname': 'Kavak',
+          'image': await File.fromUri(Uri.parse('./lib/assets/avatar.jpg'))
+              .readAsBytes(),
+        },
+      );
+      expect(response.data['message'], 'Registered successfully!');
+    } on DioException catch (error) {
+      expect(error.response?.statusCode, 400);
+      print(
+          "Test failed with 400 error code because of ${error.response?.data['errorMessage']}");
+    }
   });
 
   test("Login test", () async {
