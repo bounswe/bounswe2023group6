2c03ebb61c4fe93cd2c4c83fc8814d8603922937
Beyzanur Bektan <123978913+beyzabektan@users.noreply.github.com>
2023-10-30 23:06:57 +0300

    Frontend home page (#311)
    Co-authored-by: Ömer Talip Akalın <56600661+omertalib42@users.noreply.github.com>

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
 import { ChangePassword, ForgotPassword, Login, Signup } from './pages/Auth'
 import { HomePage } from './pages/HomePage'
 
@@ -8,6 +9,7 @@ const App = () => (
 		<Routes>
 			<Route path='/login' element={<Login />} />
 			<Route path='/forgot-password' element={<ForgotPassword />} />
+			<Route path='/home' element={<HomePage />} />
 			<Route path='/signup' element={<Signup />} />
 			<Route path='/change-password' element={<ChangePassword />} />
 			<Route path='/home' element={<HomePage />} />
