0c3372d98dd44c3cb10943747227b9e74b9717b8
Ömer Talip Akalın <56600661+omertalib42@users.noreply.github.com>
2023-10-30 22:23:52 +0300

    Implement signup/register page (#307)
    * Add signup template
    
    * Add login logic template
    
    * Add change password page
    
    * Put styling

--- app/frontend/src/pages/Auth/ForgotPassword.js
+++ app/frontend/src/pages/Auth/ForgotPassword.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import '../../index.css'
-import logo from './images/logo.png'
-import './ForgotPassword.css'
-const ForgotPassword = () => (
-	<div id='container' style={{ fontFamily: 'Jost, sans-serif' }}>
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				height: '100vh'
-			}}
-		>
+import React, { useState } from 'react'
+import 'tailwindcss/tailwind.css'
+import logo from './images/logo.png' // Make sure to import your logo
+
+const ForgotPassword = () => {
+	const [isSubmitted, setIsSubmitted] = useState(false)
+	const [email, setEmail] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		if (!email) {
+			setError('Email is required')
+			return
+		}
+
+		setIsLoading(true)
+		setError(null)
+
+		try {
+			// Simulate API call
+			await new Promise((resolve) => setTimeout(resolve, 2000))
+			setIsSubmitted(true)
+		} catch (e) {
+			setError('An error occurred. Please try again.')
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
+	return (
+		<div id='container' style={{ fontFamily: 'Jost, sans-serif', backgroundColor: '#B46060' }}>
 			<header
 				style={{
 					display: 'flex',
 					justifyContent: 'center',
 					alignItems: 'center',
-					width: '100%',
-					height: '18%',
 					backgroundColor: '#B46060',
 					borderBottom: '3px solid #B46060',
 					boxShadow: '0px 8px 4px rgba(0,0,0,0.6)'
@@ -26,58 +43,66 @@ const ForgotPassword = () => (
 			>
 				<img src={logo} alt='Game Lounge' style={{ maxHeight: '90%', maxWidth: '200px' }} />
 			</header>
-			<main
-				style={{
-					flexGrow: 1,
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
-			>
-				<div className='cover'></div>
-				<form
-					style={{
-						backgroundColor: '#4D4D4Dee',
-						boxShadow: '0px 4px 15px rgba(0,0,0,0.8)'
-					}}
+			<div className='bg-gray-200 py-20' style={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+				<div
+					className='max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-md'
+					style={{ backgroundColor: '#4D4D4Dee', boxShadow: '0px 4px 15px rgba(0,0,0,0.8)' }}
 				>
-					<h1 style={{ color: '#FFF4E0', fontSize: '1.4rem' }}>Forgot Password</h1>
-					<h2 style={{ color: '#FFF4E0', fontSize: '1rem' }}>
-						If the email you enter matches an account, we will send a reset code to:
-					</h2>
-
-					<input
-						id='input-email'
-						type='email'
-						placeholder='Enter your email'
-						style={{
-							borderRadius: '3px',
-							outline: 'none',
-							fontSize: '1rem',
-							boxShadow: '0px 4px 15px rgba(0,0,0,0.6)'
-						}}
-					/>
-					<div id='btn-container'>
-						<button
-							type='submit'
-							style={{
-								backgroundColor: '#FFBF9B',
-								color: '#B46060',
-								border: 'none',
-								borderRadius: '3px',
-								cursor: 'pointer',
-								fontSize: '1rem',
-								fontWeight: 600,
-								boxShadow: '0px 4px 15px rgba(0,0,0,0.6)'
-							}}
-						>
-							Reset Password
-						</button>
+					<div className='md:flex'>
+						<div className='w-full p-3 px-6 py-10'>
+							<div className='w-full'>
+								{isSubmitted ? (
+									<div>
+										<h1 className='text-xl text-gray-700 font-semibold mb-2'>Reset link sent!</h1>
+										<p className='text-gray-500'>Please check your email for a link to reset your password.</p>
+									</div>
+								) : (
+									<form onSubmit={handleSubmit} className='space-y-5'>
+										<h1 style={{ color: '#FFF4E0', fontSize: '1.4rem' }}>Forgot Password</h1>
+										<h2 style={{ color: '#FFF4E0', fontSize: '1rem' }}>
+											If the email you enter matches an account, we will send a reset code to:
+										</h2>
+										<div>
+											<label htmlFor='email' className='block text-sm font-medium text-gray-600'>
+												Email
+											</label>
+											<input
+												type='email'
+												id='email'
+												name='email'
+												value={email}
+												onChange={(e) => setEmail(e.target.value)}
+												className='mt-1 p-2 w-full border rounded-md'
+												style={{
+													borderRadius: '3px',
+													outline: 'none',
+													fontSize: '1rem',
+													boxShadow: '0px 4px 15px rgba(0,0,0,0.6)'
+												}}
+											/>
+										</div>
+										{error && <p className='text-red-500'>{error}</p>}
+										<button
+											type='submit'
+											className='w-full bg-blue-500 text-white p-2 rounded-md'
+											style={{
+												backgroundColor: '#FFBF9B',
+												color: '#B46060',
+												boxShadow: '0px 4px 15px rgba(0,0,0,0.6)'
+											}}
+											disabled={isLoading}
+										>
+											{isLoading ? 'Loading...' : 'Send Reset Link'}
+										</button>
+									</form>
+								)}
+							</div>
+						</div>
 					</div>
-				</form>
-			</main>
+				</div>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
 
 export default ForgotPassword
--- app/frontend/src/pages/Auth/Login.js
+++ app/frontend/src/pages/Auth/Login.js
@@ -1,114 +1,191 @@
 import React, { useState } from 'react'
-import VisibilityIcon from '@mui/icons-material/Visibility'
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-import { Container, TextField, Button, InputAdornment, IconButton, Link } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Alert from '@mui/material/Alert'
-import Grid from '@mui/material/Grid'
-const LoginPage = () => {
-	const [showPassword, setShowPassword] = useState(false)
+import { useNavigate } from 'react-router-dom'
+import 'tailwindcss/tailwind.css'
 
-	const handlePasswordVisibility = () => {
-		setShowPassword(!showPassword)
-	}
-	const [isError, setError] = useState(null)
+const Login = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+	const navigate = useNavigate()
 
-	const defaultTheme = createTheme()
+	const handleLogin = async (e) => {
+		e.preventDefault()
+		setIsLoading(true)
+		setError(null)
 
-	const handleSubmit = (event) => {
-		event.preventDefault()
-		const data = new FormData(event.currentTarget)
-		//TODO
-		if (data.get('email') === 'admin' && data.get('password') === 'admin') {
-			setError(false)
-		} else {
-			setError(true)
+		try {
+			// Simulate API call for login
+			await new Promise((resolve, reject) => {
+				setTimeout(() => {
+					if (email === 'user@example.com' && password === 'password') {
+						resolve()
+					} else {
+						reject(new Error('Invalid credentials'))
+					}
+				}, 2000)
+			})
+
+			navigate('/') // Redirect to homepage
+		} catch (err) {
+			setError('Invalid credentials. Please try again.')
+		} finally {
+			setIsLoading(false)
 		}
-		console.log({
-			email: data.get('email'),
-			password: data.get('password')
-		})
 	}
 
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Container component='main' maxWidth='xs'>
-				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 20,
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'center'
-					}}
-				>
-					<Typography component='h1' variant='h5'>
-						Sign in
-					</Typography>
-
-					<Container maxWidth='sm'>
-						{isError && (
-							<Alert variant='filled' severity='error' sx={{ mt: 1 }}>
-								Username or password is wrong!
-							</Alert>
-						)}
-
-						<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-							<TextField
-								margin='normal'
-								required
-								fullWidth
-								id='email'
-								label='Email Address'
-								name='email'
-								autoComplete='email'
-								autoFocus
-							/>
-							<TextField
-								margin='normal'
-								required
-								fullWidth
-								name='password'
-								label='Password'
-								type={showPassword ? 'text' : 'password'}
-								id='password'
-								autoComplete='current-password'
-								InputProps={{
-									endAdornment: (
-										<InputAdornment position='end'>
-											<IconButton onClick={handlePasswordVisibility}>
-												{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-											</IconButton>
-										</InputAdornment>
-									)
-								}}
-							/>
-
-							<Button type='submit' fullWidth variant='contained' sx={{ mt: 1, mb: 2 }}>
-								Sign In
-							</Button>
-							<Grid container>
-								<Grid item xs>
-									<Link href='forgot-password' variant='body2'>
-										Forgot password?
-									</Link>
-								</Grid>
-								<Grid item>
-									<Link href='#' variant='body2'>
-										{"Don't have an account? Sign Up"}
-									</Link>
-								</Grid>
-							</Grid>
-						</Box>
-					</Container>
-				</Box>
-			</Container>
-		</ThemeProvider>
+		<div className='bg-gray-200 py-20'>
+			<div className='max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-md'>
+				<div className='md:flex'>
+					<div className='w-full p-3 px-6 py-10'>
+						<div className='w-full'>
+							<h1 className='text-xl text-gray-700 font-semibold mb-2'>Log in to your account</h1>
+							<form onSubmit={handleLogin} className='space-y-5'>
+								<div>
+									<label className='block mb-1 font-semibold text-gray-500'>Email</label>
+									<input
+										className='w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline'
+										type='email'
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
+									/>
+								</div>
+								<div>
+									<label className='block mb-1 font-semibold text-gray-500'>Password</label>
+									<input
+										className='w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline'
+										type='password'
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+								{error && <p className='text-red-500'>{error}</p>}
+								<button
+									className='w-full h-10 px-3 text-white transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700'
+									type='submit'
+									disabled={isLoading}
+								>
+									{isLoading ? 'Logging in...' : 'Log in'}
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
+// import VisibilityIcon from '@mui/icons-material/Visibility'
+// import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+// import { Container, TextField, Button, InputAdornment, IconButton, Link } from '@mui/material'
+// import { createTheme, ThemeProvider } from '@mui/material/styles'
+// import CssBaseline from '@mui/material/CssBaseline'
+// import Box from '@mui/material/Box'
+// import Typography from '@mui/material/Typography'
+// import Alert from '@mui/material/Alert'
+// import Grid from '@mui/material/Grid'
+// const LoginPage = () => {
+// 	const [showPassword, setShowPassword] = useState(false)
 
-export default LoginPage
+// 	const handlePasswordVisibility = () => {
+// 		setShowPassword(!showPassword)
+// 	}
+// 	const [isError, setError] = useState(null)
+
+// 	const defaultTheme = createTheme()
+
+// 	const handleSubmit = (event) => {
+// 		event.preventDefault()
+// 		const data = new FormData(event.currentTarget)
+// 		//TODO
+// 		if (data.get('email') === 'admin' && data.get('password') === 'admin') {
+// 			setError(false)
+// 		} else {
+// 			setError(true)
+// 		}
+// 		console.log({
+// 			email: data.get('email'),
+// 			password: data.get('password')
+// 		})
+// 	}
+
+// 	return (
+// 		<ThemeProvider theme={defaultTheme}>
+// 			<Container component='main' maxWidth='xs'>
+// 				<CssBaseline />
+// 				<Box
+// 					sx={{
+// 						marginTop: 20,
+// 						display: 'flex',
+// 						flexDirection: 'column',
+// 						justifyContent: 'center',
+// 						alignItems: 'center'
+// 					}}
+// 				>
+// 					<Typography component='h1' variant='h5'>
+// 						Sign in
+// 					</Typography>
+
+// 					<Container maxWidth='sm'>
+// 						{isError && (
+// 							<Alert variant='filled' severity='error' sx={{ mt: 1 }}>
+// 								Username or password is wrong!
+// 							</Alert>
+// 						)}
+
+// 						<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+// 							<TextField
+// 								margin='normal'
+// 								required
+// 								fullWidth
+// 								id='email'
+// 								label='Email Address'
+// 								name='email'
+// 								autoComplete='email'
+// 								autoFocus
+// 							/>
+// 							<TextField
+// 								margin='normal'
+// 								required
+// 								fullWidth
+// 								name='password'
+// 								label='Password'
+// 								type={showPassword ? 'text' : 'password'}
+// 								id='password'
+// 								autoComplete='current-password'
+// 								InputProps={{
+// 									endAdornment: (
+// 										<InputAdornment position='end'>
+// 											<IconButton onClick={handlePasswordVisibility}>
+// 												{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+// 											</IconButton>
+// 										</InputAdornment>
+// 									)
+// 								}}
+// 							/>
+
+// 							<Button type='submit' fullWidth variant='contained' sx={{ mt: 1, mb: 2 }}>
+// 								Sign In
+// 							</Button>
+// 							<Grid container>
+// 								<Grid item xs>
+// 									<Link href='forgot-password' variant='body2'>
+// 										Forgot password?
+// 									</Link>
+// 								</Grid>
+// 								<Grid item>
+// 									<Link href='#' variant='body2'>
+// 										{"Don't have an account? Sign Up"}
+// 									</Link>
+// 								</Grid>
+// 							</Grid>
+// 						</Box>
+// 					</Container>
+// 				</Box>
+// 			</Container>
+// 		</ThemeProvider>
+// 	)
+// }
+
+export default Login
