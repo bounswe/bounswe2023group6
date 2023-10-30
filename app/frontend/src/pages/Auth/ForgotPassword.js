d7c05fbae931d7cb0579a15ab2161f8d8be4be87
Ömer Talip Akalın <omertalib42@gmail.com>
2023-10-30 19:18:39 +0300

    Add login logic template

--- app/frontend/src/pages/Auth/ForgotPassword.js
+++ app/frontend/src/pages/Auth/ForgotPassword.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'tailwindcss/tailwind.css'
 
-const ForgotPassword = () => (
-	<form>
-		<h1>Forgot Password</h1>
-		<input type='email' placeholder='Enter your email' />
-		<button type='submit'>Reset Password</button>
-	</form>
-)
+const ForgotPassword = () => {
+	const [isSubmitted, setIsSubmitted] = useState(false)
+	const [email, setEmail] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		if (!email) {
+			setError('Email is required')
+			return
+		}
+
+		setIsLoading(true)
+		setError(null)
+
+		try {
+			// Simulate API call
+			await new Promise((resolve) => setTimeout(resolve, 2000))
+			setIsSubmitted(true)
+		} catch (e) {
+			setError('An error occurred. Please try again.')
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
+	return (
+		<div className='bg-gray-200 py-20'>
+			<div className='max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-md'>
+				<div className='md:flex'>
+					<div className='w-full p-3 px-6 py-10'>
+						<div className='w-full'>
+							{isSubmitted ? (
+								<div>
+									<h1 className='text-xl text-gray-700 font-semibold mb-2'>Reset link sent!</h1>
+									<p className='text-gray-500'>Please check your email for a link to reset your password.</p>
+								</div>
+							) : (
+								<form onSubmit={handleSubmit} className='space-y-5'>
+									<div>
+										<label htmlFor='email' className='block text-sm font-medium text-gray-600'>
+											Email
+										</label>
+										<input
+											type='email'
+											id='email'
+											name='email'
+											value={email}
+											onChange={(e) => setEmail(e.target.value)}
+											className='mt-1 p-2 w-full border rounded-md'
+										/>
+									</div>
+									{error && <p className='text-red-500'>{error}</p>}
+									<button type='submit' className='w-full bg-blue-500 text-white p-2 rounded-md' disabled={isLoading}>
+										{isLoading ? 'Loading...' : 'Send Reset Link'}
+									</button>
+								</form>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export default ForgotPassword
--- app/frontend/src/pages/Auth/Login.js
+++ app/frontend/src/pages/Auth/Login.js
@@ -1,15 +1,80 @@
-import React from 'react'
-import '../../index.css'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import 'tailwindcss/tailwind.css'
 
-const Login = () => (
-	<form>
-		<h1>Login</h1>
-		<input type='text' placeholder='Username' />
-		<input type='password' placeholder='Password' />
-		<button className='btn' type='submit'>
-			Login
-		</button>
-	</form>
-)
+const Login = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+	const navigate = useNavigate()
+
+	const handleLogin = async (e) => {
+		e.preventDefault()
+		setIsLoading(true)
+		setError(null)
+
+		try {
+			// Simulate API call for login
+			await new Promise((resolve, reject) => {
+				setTimeout(() => {
+					if (email === 'user@example.com' && password === 'password') {
+						resolve()
+					} else {
+						reject(new Error('Invalid credentials'))
+					}
+				}, 2000)
+			})
+
+			navigate('/') // Redirect to homepage
+		} catch (err) {
+			setError('Invalid credentials. Please try again.')
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
+	return (
+		<div className='bg-gray-200 py-20'>
+			<div className='max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-md'>
+				<div className='md:flex'>
+					<div className='w-full p-3 px-6 py-10'>
+						<div className='w-full'>
+							<h1 className='text-xl text-gray-700 font-semibold mb-2'>Log in to your account</h1>
+							<form onSubmit={handleLogin} className='space-y-5'>
+								<div>
+									<label className='block mb-1 font-semibold text-gray-500'>Email</label>
+									<input
+										className='w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline'
+										type='email'
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
+									/>
+								</div>
+								<div>
+									<label className='block mb-1 font-semibold text-gray-500'>Password</label>
+									<input
+										className='w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline'
+										type='password'
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+								{error && <p className='text-red-500'>{error}</p>}
+								<button
+									className='w-full h-10 px-3 text-white transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700'
+									type='submit'
+									disabled={isLoading}
+								>
+									{isLoading ? 'Logging in...' : 'Log in'}
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export default Login
