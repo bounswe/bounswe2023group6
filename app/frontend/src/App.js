f9976d459900d0814c99c2e370c9f0804decf40a
Ömer Talip Akalın <omertalib42@gmail.com>
2023-10-30 18:11:53 +0300

    Add signup template

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import { Login, ForgotPassword } from './pages/Auth'
+import { ForgotPassword, Login, Signup } from './pages/Auth'
 
 const App = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path='/login' element={<Login />} />
 			<Route path='/forgot-password' element={<ForgotPassword />} />
+			<Route path='/signup' element={<Signup />} />
 		</Routes>
 	</BrowserRouter>
 )
