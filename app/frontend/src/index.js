6259f23d51aee19ba2c41c82fe82dab789ffea41
Halis Ayberk Erdem <ayberk.erdem@hotmail.com>
2023-10-30 19:57:16 +0300

    login first template

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -7,16 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
 	<React.StrictMode>
-		<>
-			{/* tailwind hello world */}
-			<div className='flex h-0.25'>
-				<div className='text-4xl font-bold text-center text-blue-600'>Hello World!</div>
-			</div>
-			{/* tailwind cool button that changes color on hover */}
-			<div className='flex h-0.5'>
-				<button className='px-12 py-2 font-bold text-white bg-blue-600 rounded hover:bg-green-800'>Click Me!</button>
-			</div>
-		</>
+		<>{/* tailwind hello world */}</>
 		<App />
 	</React.StrictMode>
 )
