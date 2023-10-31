54acdb6219be02181fb79898bd077c5ab7cc5df0
Ömer Talip Akalın <omertalib42@gmail.com>
2023-10-31 12:07:25 +0300

    Refactor home page

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { ResetPassword, ForgotPassword, Login, Signup } from './pages/Auth'
-// import { HomePage } from './pages/HomePage'
 
 const App = () => (
 	<BrowserRouter>
 		<Routes>
+			<Route exact path="/" element={<HomePage />} />
 			<Route path='/login' element={<Login />} />
 			<Route path='/forgot-password' element={<ForgotPassword />} />
-			{/* <Route path='/home' element={<HomePage />} /> */}
 			<Route path='/signup' element={<Signup />} />
 			<Route path='/reset-password' element={<ResetPassword />} />
 			<Route path='/home' element={<HomePage />} />
