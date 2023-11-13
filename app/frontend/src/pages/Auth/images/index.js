5ae876f15e78b1eba6a0ffb5230ded034f349118
Beyzanur Bektan <beyzabektan@gmail.com>
2023-11-13 14:36:09 +0300

    added index.js into the images folder

--- app/frontend/src/pages/Auth/images/index.js
+++ app/frontend/src/pages/Auth/images/index.js
@@ -1,13 +1,13 @@
-import kerimbahadir from "./kerimbahadir.jpg"
-import alikasap from "./alikasap.jpg"
-import zehrayildirim from "./zehrayildirim.jpg"
-import mahmutdemir from "./mahmutdemir.jpg"
-import fifa from "./fifa.jpg"
-import fortnite from "./fortnite.jpg"
-import pubg from "./pubg.jpg"
-import residentevil from "./residentevil.jpg"
-import rocketleague from "./rocketleague.jpg"
-import witcher from "./witcher.jpg"
+import kerimbahadir from "./kerimbahadir.jpg";
+import alikasap from "./alikasap.jpg";
+import zehrayildirim from "./zehrayildirim.jpg";
+import mahmutdemir from "./mahmutdemir.jpg";
+import fifa from "./fifa.jpg";
+import fortnite from "./fortnite.jpg";
+import pubg from "./pubg.jpg";
+import residentevil from "./residentevil.jpg";
+import rocketleague from "./rocketleague.jpg";
+import witcher from "./witcher.jpg";
 
 export {
    kerimbahadir,
@@ -20,4 +20,4 @@ export {
    residentevil,
    rocketleague,
    witcher
-   }
+   };
