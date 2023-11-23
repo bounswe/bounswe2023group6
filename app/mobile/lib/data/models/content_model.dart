24a821fff5b8eb9be6f70b948965f3efcfae357a
Erkam Kavak <erkam.kavak@boun.edu.tr>
2023-11-24 01:55:26 +0300

    content, post and comment models modified based on backend

--- app/mobile/lib/data/models/content_model.dart
+++ app/mobile/lib/data/models/content_model.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/data/models/comment_model.dart';
 import 'package:mobile/data/models/user_model.dart';
+import 'package:mobile/presentation/widgets/avatar_widget.dart';
 
 enum ContentType { post, comment, lfg }
 
 class Content {
   final DateTime createdDate;
   final int id;
-  final String content;
+  String content;
   final ContentType type;
-  final int userId;
-  final String username; 
+  User ownerUser;
+  // final int userId;
+  // final String username;
   // final List<String>? annotations;
   // final List<String>? tags;
   // final List<Report> reports;
@@ -29,16 +31,18 @@ class Content {
   List<int> commentIds = [];
 
   List<Comment> commentList = [];
+  int? parentContentId;
 
-  // User? ownerUser;
+  int? relatedGameId;
   String? title; // For post and lfg
 
   Content({
     required this.id,
     required this.content,
     required this.type,
-    required this.userId,
-    required this.username,
+    // required this.userId,
+    // required this.username,
+    required this.ownerUser,
     required this.createdDate,
     // this.annotations,
     // this.tags,
@@ -49,36 +53,62 @@ class Content {
     this.title,
     this.likes = 0,
     this.dislikes = 0,
-    this.comments = 0,
     this.shares = 0,
     this.views = 0,
-  });
+    this.parentContentId,
+    this.relatedGameId,
+    this.commentList = const [],
+  }) : comments = commentList.length;
 
   // Content initialize
 }
 
-
 // Content related widgets
-Widget userInformationSection(Content content) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          children: [
-            const Icon(Icons.account_circle),
-            SizedBox(
-              width: 50,
-              child: Padding(
+Widget userInformationSection(BuildContext context, User user,
+    {bool isContentOfOriginalPoster = false}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Column(
+        children: [
+          // user.profileImage != null
+          //     ? DisplayAvatar(
+          //         byteData: user.profileImage,
+          //         onPressed: () {
+          //           // go to profile page
+          //           Navigator.pushNamed(context, '/profile', arguments: username);
+          //         },
+          //       )
+          //     : const Icon(Icons.account_circle),
+          IconButton(
+            onPressed: () {
+              // go to profile page
+              Navigator.pushNamed(context, '/profile', arguments: user.username);
+            },
+            icon: const Icon(Icons.account_circle),
+          ),
+          SizedBox(
+            width: 80,
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(user.username!, style: const TextStyle(fontSize: 13)),
+              ),
+            ),
+          ),
+          isContentOfOriginalPoster 
+          // Show original poster tag(in a colored button) if content is of original poster
+            ? Padding(
                 padding: const EdgeInsets.all(4.0),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: Text(content.username,
-                      style: const TextStyle(fontSize: 13)),
+                  child: Text('Original Poster', style: TextStyle(fontSize: 13, color: Theme.of(context).cardColor)),
                 ),
-              ),
-            )
-          ],
-        ),
-      ],
-    );
-  }
+              ) 
+            : Container(),
+        ],
+      ),
+    ],
+  );
+}
--- app/mobile/lib/data/models/post_model.dart
+++ app/mobile/lib/data/models/post_model.dart
@@ -1,26 +1,59 @@
+import 'package:mobile/data/models/comment_model.dart';
 import 'package:mobile/data/models/content_model.dart';
+import 'package:mobile/data/models/user_model.dart';
 
 class Post extends Content {
   Post({
     required DateTime createdDate,
     required int id,
     required String content,
-    required int userId,
-    required username,
+    required ownerUser,
     required title,
+    required relatedGameId, 
     likes = 0,
     dislikes = 0,
     comments = 0,
+    List<Comment> commentList = const [],
   }) : super(
     createdDate: createdDate,
     id: id,
     content: content,
     type: ContentType.post,
-    userId: userId,
     likes: likes,
     dislikes: dislikes,
-    comments: comments,
+    ownerUser: ownerUser,
+    commentList: commentList,
     title: title,
-    username: username, 
+    relatedGameId: relatedGameId, 
   );
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['postId'],
+      title: json['title'],
+      content: json['content'],
+      createdDate: DateTime.parse(json['creationDate']),
+      likes: json['upvotes'],
+      dislikes: json['downvotes'],
+      ownerUser: User.fromJson(json['user']), 
+      commentList: List<Comment>.from(json['comments'].map((x) => Comment.fromJson(x))),
+      relatedGameId: json['game']['gameId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'postId': id,
+      'title': title,
+      'content': content,
+      'creationDate': createdDate.toString(),
+      'upvotes': likes,
+      'downvotes': dislikes,
+      'user': ownerUser.toJson(),
+      'comments': commentList.map((x) => x.toJson()),
+      'game': {
+        'gameId': relatedGameId,
+      },
+    };
+  }
 }
