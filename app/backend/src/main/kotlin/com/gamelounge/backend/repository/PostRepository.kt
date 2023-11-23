0f7db137ad9b8bc62e292d5ccf9178eef50e2529
Legervad <emre.turker@boun.edu.tr>
2023-11-23 17:34:33 +0300

    #437 minor changes on nullability issues

--- app/backend/src/main/kotlin/com/gamelounge/backend/repository/PostRepository.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/repository/PostRepository.kt
@@ -1,6 +1,7 @@
 package com.gamelounge.backend.repository
 import org.springframework.stereotype.Repository
 import com.gamelounge.backend.entity.Post
+import com.gamelounge.backend.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 @Repository
 interface PostRepository : JpaRepository<Post, Long>{
