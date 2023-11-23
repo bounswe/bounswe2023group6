048f4ecdc401efcd237a38e609837a8b4b79f048
Legervad <emre.turker@boun.edu.tr>
2023-11-23 17:34:10 +0300

    #437 implement report related entity and dto

---
+++ app/backend/src/main/kotlin/com/gamelounge/backend/model/DTO/ReportDTO.kt
@@ -0,0 +1,10 @@
+package com.gamelounge.backend.model.DTO
+
+data class ReportDTO(
+    val reportId: Long = 0,
+    val reason: String = "",
+    var reportingUser: UserDTO,
+    var reportedPost: PostDTO? = null,
+    var reportedLFG: LFGDTO? = null,
+    var reportedComment: CommentDTO? = null
+)
