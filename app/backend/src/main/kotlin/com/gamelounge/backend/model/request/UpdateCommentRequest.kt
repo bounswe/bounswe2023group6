a3e39604177577484d207cc51946da8b60b65db3
Omer Bahadiroglu <93478016+ohbahadiroglu@users.noreply.github.com>
2023-11-25 00:24:08 +0300

    some request model bug fixes

--- app/backend/src/main/kotlin/com/gamelounge/backend/model/request/UpdateCommentRequest.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/model/request/UpdateCommentRequest.kt
@@ -1,5 +1,7 @@
 package com.gamelounge.backend.model.request
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UpdateCommentRequest(
+    @JsonProperty("content")
     val content: String
 )
