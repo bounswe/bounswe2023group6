1d04b9b7e317a48071ff066b2d1e32a54bcbf3b5
Emre <74266381+Legervad@users.noreply.github.com>
2023-11-24 16:06:55 +0300

    #392 implement aws s3 features (#442)

---
+++ app/backend/src/main/kotlin/com/gamelounge/backend/constant/S3Constants.kt
@@ -0,0 +1,6 @@
+package com.gamelounge.backend.constant
+
+object S3Constants {
+    const val PROFILE_PICTURE_DIRECTORY = "https://game-lounge-storage.s3.eu-north-1.amazonaws.com/profile-pictures/"
+    const val GAME_PICTURE_DIRECTORY = "https://game-lounge-storage.s3.eu-north-1.amazonaws.com/game-pictures/"
+}
