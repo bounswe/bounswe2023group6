939b4ac77cf5fa5640abecf22d529c47912e5aec
Omer Bahadiroglu <93478016+ohbahadiroglu@users.noreply.github.com>
2023-11-26 14:22:00 +0300

    removed unnecessary cross origin permission statements

--- app/backend/src/main/kotlin/com/gamelounge/backend/controller/PostController.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/controller/PostController.kt
@@ -18,7 +18,6 @@ import java.util.UUID
 class PostController(private val postService: PostService) {
 
     @PostMapping
-    @CrossOrigin(origins = ["*"])
     fun createPost(@CookieValue("SESSIONID") sessionId: UUID, @RequestBody post: CreatePostRequest): ResponseEntity<PostDTO> {
         val newPost = postService.createPost(sessionId, post)
         val newPostDTO = ConverterDTO.convertToPostDTO(newPost)
@@ -26,7 +25,6 @@ class PostController(private val postService: PostService) {
     }
 
     @GetMapping("/{id}")
-    @CrossOrigin(origins = ["*"])
     fun getPost(@PathVariable id: Long): ResponseEntity<PostDTO> {
         val post = postService.getPost(id)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -34,7 +32,6 @@ class PostController(private val postService: PostService) {
     }
 
     @PutMapping("/{id}")
-    @CrossOrigin(origins = ["*"])
     fun updatePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long, @RequestBody updatedPost: UpdatePostRequest): ResponseEntity<PostDTO> {
         val post = postService.updatePost(sessionId, id, updatedPost)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -42,14 +39,12 @@ class PostController(private val postService: PostService) {
     }
 
     @DeleteMapping("/{id}")
-    @CrossOrigin(origins = ["*"])
     fun deletePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<ResponseMessage> {
         postService.deletePost(sessionId, id)
         return ResponseEntity.ok(ResponseMessage(message = "Post deleted successfully"))
     }
 
     @GetMapping
-    @CrossOrigin(origins = ["*"])
     fun getAllPosts(): ResponseEntity<List<PostDTO>> {
         val posts = postService.getAllPosts()
         val postsDTO = ConverterDTO.convertBulkToPostDTO(posts)
@@ -58,7 +53,6 @@ class PostController(private val postService: PostService) {
 
     // implement upvote endpoint
     @PutMapping("/{id}/upvote")
-    @CrossOrigin(origins = ["*"])
     fun upvotePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<PostDTO> {
         val post = postService.upvotePost(sessionId, id)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -67,7 +61,6 @@ class PostController(private val postService: PostService) {
 
     // implement downvote endpoint
     @PutMapping("/{id}/downvote")
-    @CrossOrigin(origins = ["*"])
     fun downvotePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<PostDTO> {
         val post = postService.downvotePost(sessionId, id)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -76,7 +69,6 @@ class PostController(private val postService: PostService) {
 
     // implement get upvoted users endpoint
     @GetMapping("/{id}/upvoteUsers")
-    @CrossOrigin(origins = ["*"])
     fun getUpvotedUsers(@PathVariable id: Long): ResponseEntity<List<UserDTO>> {
         val upvotedUsersDTO = postService.getUpvotedUsers(id)
         return ResponseEntity.ok(upvotedUsersDTO)
@@ -84,14 +76,12 @@ class PostController(private val postService: PostService) {
 
     // implement get downvoted users endpoint
     @GetMapping("/{id}/downvoteUsers")
-    @CrossOrigin(origins = ["*"])
     fun getDownvotedUsers(@PathVariable id: Long): ResponseEntity<List<UserDTO>> {
         val downvotedUsersDTO = postService.getDownvotedUsers(id)
         return ResponseEntity.ok(downvotedUsersDTO)
     }
     // REPORT POST
     @PostMapping("/{id}/report")
-    @CrossOrigin(origins = ["*"])
     fun reportPost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long, @RequestBody reqBody: ReportRequest): ResponseEntity<ResponseMessage> {
         postService.reportPost(sessionId, id, reqBody)
         return ResponseEntity.ok(ResponseMessage(message = "Post reported successfully"))
