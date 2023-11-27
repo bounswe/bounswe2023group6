e66cf81958331ead7a84376609c6c805678ce5b6
Ömer Talip Akalın <omertalib42@gmail.com>
2023-11-28 02:03:54 +0300

    Fix: Make title clickable

--- app/frontend/src/pages/HomePage/Post.js
+++ app/frontend/src/pages/HomePage/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Post = ({ post }) => {
     return (
@@ -7,7 +8,11 @@ const Post = ({ post }) => {
                 <img src={post.creatorUser.profilePicture} alt="Post" className="rounded-lg h-20 w-20 object-cover" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title text-gray-700">{post.title}</h2>
+                <h2 className="card-title text-gray-700">
+                    <Link to={`/posts/${post.postId}`}>
+                        {post.title}
+                    </Link>
+                </h2>
                 <p className="text-xs">{post.content}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-circle btn-sm bg-[#b46161] border-[#b46161] text-gray-100 hover:bg-[#8c4646] hover:border-[#8c4646]">
