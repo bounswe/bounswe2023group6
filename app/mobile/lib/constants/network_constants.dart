e4d94fb10f1dcb5911b9434ce1df0a841e586071
Mustafa Küçük <44123646+mustafakucuk-dev@users.noreply.github.com>
2023-11-27 19:55:59 +0300

    Update network_constants.dart

--- app/mobile/lib/constants/network_constants.dart
+++ app/mobile/lib/constants/network_constants.dart
@@ -1,6 +1,6 @@
 class NetworkConstants {
   static const String BASE_PROD_URL = "http://167.99.242.175:8080";
-  static const String BASE_LOCAL_URL = "http://167.99.242.175:8080";
+  static const String BASE_LOCAL_URL = "http://localhost:8080";
 
   static const String defaultGameImage =
       "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg";
