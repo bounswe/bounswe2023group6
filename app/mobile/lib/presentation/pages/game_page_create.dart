55562a14c0eba76075ac3cd3ef119646d4942f8d
umutdmr315 <umutdmr315@gmail.com>
2023-11-27 15:21:03 +0300

    Create game page.

--- app/mobile/lib/presentation/pages/game_page_create.dart
+++ app/mobile/lib/presentation/pages/game_page_create.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/constants/color_constants.dart';
 import 'package:mobile/data/models/game_model.dart';
-import 'package:mobile/data/services/post_service.dart';
+import 'package:mobile/data/services/game_service.dart';
+import 'package:mobile/presentation/widgets/button_widget.dart';
 
 class GamePageCreate extends StatefulWidget {
   const GamePageCreate({Key? key}) : super(key: key);
@@ -12,13 +14,18 @@ class GamePageCreate extends StatefulWidget {
 class _GameCreatePageState extends State<GamePageCreate> {
   final _formKey = GlobalKey<FormState>();
   final _contentController = TextEditingController();
-  final _titleController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _imageController = TextEditingController();
+  final _genreController = TextEditingController();
+  final _developerController = TextEditingController();
+  final _yearController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Create Game Page"),
+        backgroundColor: ColorConstants.color1,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -27,7 +34,7 @@ class _GameCreatePageState extends State<GamePageCreate> {
             key: _formKey,
             child: Column(children: [
               TextFormField(
-                controller: _titleController,
+                controller: _nameController,
                 decoration: const InputDecoration(
                   hintText: "Name",
                 ),
@@ -52,18 +59,71 @@ class _GameCreatePageState extends State<GamePageCreate> {
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    await PostService().createPost(
-                        _titleController.text, _contentController.text);
-                    Navigator.of(context).pop("create");
+              TextFormField(
+                controller: _imageController,
+                decoration: const InputDecoration(
+                  hintText: "Image",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter an image";
                   }
+                  return null;
                 },
-                child: const Text("Create"),
               ),
+              TextFormField(
+                controller: _genreController,
+                decoration: const InputDecoration(
+                  hintText: "Genre",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a genre";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _developerController,
+                decoration: const InputDecoration(
+                  hintText: "Developer",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a developer";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _yearController,
+                decoration: const InputDecoration(
+                  hintText: "Year",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a year";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              const SizedBox(height: 16),
+              Button(
+                  label: "Create",
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      GameService.addGame(Game(
+                          id: GameService.getGameDataList().length + 1,
+                          name: _nameController.text,
+                          description: _contentController.text,
+                          imageLink: _imageController.text,
+                          genre: _genreController.text,
+                          developers: _developerController.text,
+                          releaseYear: _yearController.text));
+                      Navigator.of(context).pop("create");
+                    }
+                  }),
             ]),
           ),
         ),
