9bdb8fe16839ab36068df8518fac56c8d2aef111
Ömer Talip Akalın <56600661+omertalib42@users.noreply.github.com>
2023-11-28 03:26:26 +0300

    Fix .env (#488)

--- app/frontend/src/services/gameServise.js
+++ app/frontend/src/services/gameServise.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_API_URL
 })
 axiosInstance.defaults.withCredentials = true
 
