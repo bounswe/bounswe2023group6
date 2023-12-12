8bb2cbdb96668fd70979eb2e3e2cab74c3c49113
ahmetkudu <74921213+ahmetkudu@users.noreply.github.com>
2023-12-12 20:32:00 +0300

    completed creating game field enumerations (#538)

--- app/backend/src/main/kotlin/com/gamelounge/backend/entity/Game.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/entity/Game.kt
@@ -5,11 +5,15 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
+import com.gamelounge.backend.constant.GameGenre
+import com.gamelounge.backend.constant.GamePlatform
+import com.gamelounge.backend.constant.NumberOfPlayers
+import com.gamelounge.backend.constant.UniverseInfo
+import com.gamelounge.backend.constant.GameMechanics
 
 import jakarta.persistence.*
 import lombok.NoArgsConstructor
 import java.time.Instant
-
 @Entity
 @Table(name = "games")
 @NoArgsConstructor
@@ -20,12 +24,17 @@ class Game(
 
         var title: String = "",
         var description: String = "",
-        var genre: String = "",
-        var platform: String = "",
-        var playerNumber: String = "",
+        @Enumerated(EnumType.STRING)
+        var genre: GameGenre = GameGenre.EMPTY,
+        @Enumerated(EnumType.STRING)
+        var platform: GamePlatform = GamePlatform.EMPTY,
+        @Enumerated(EnumType.STRING)
+        var playerNumber: NumberOfPlayers = NumberOfPlayers.EMPTY,
         var releaseYear: Int = 0,
-        var universe: String = "",
-        var mechanics: String = "",
+        @Enumerated(EnumType.STRING)
+        var universe: UniverseInfo = UniverseInfo.EMPTY,
+        @Enumerated(EnumType.STRING)
+        var mechanics: GameMechanics = GameMechanics.EMPTY,
         var playtime: String = "",
         var totalRating: Int = 0,
         var countRating: Int = 0,
