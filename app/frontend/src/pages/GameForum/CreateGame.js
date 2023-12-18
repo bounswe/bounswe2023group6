80be823fcb6682c56c36a9730da53e262c7b6a9c
Beyzanur Bektan <beyzabektan@gmail.com>
2023-12-18 14:44:54 +0300

    general styling

--- app/frontend/src/pages/GameForum/CreateGame.js
+++ app/frontend/src/pages/GameForum/CreateGame.js
@@ -157,7 +157,7 @@ export default function CreateGame() {
 	return (
 		<div>
 			<Button variant='outlined' onClick={handleClickOpen}
-            			 sx={{ color: 'white', backgroundColor: '#b46161', border: 'none', '&:hover': { backgroundColor: '#6e4141', border: 'none'} }}>
+            			 sx={{ color: 'white', backgroundColor: '#b46161', border: 'none', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)', '&:hover': { backgroundColor: '#6e4141', border: 'none'} }}>
             				Create Post
             			</Button>
 			<Dialog open={open} onClose={handleClose} fullWidth>
