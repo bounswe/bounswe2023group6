f4bf178e49d30a207684bed60fc45fb5c061e002
Legervad <emre.turker@boun.edu.tr>
2023-12-22 17:19:43 +0300

    #567 implement getRecommendedPosts endpoint

--- app/backend/src/main/kotlin/com/gamelounge/backend/controller/PostController.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/controller/PostController.kt
@@ -1,6 +1,7 @@
 package com.gamelounge.backend.controller
 
 import com.gamelounge.backend.entity.Post
+import com.gamelounge.backend.entity.User
 import com.gamelounge.backend.model.DTO.PostDTO
 import com.gamelounge.backend.model.DTO.UserDTO
 import com.gamelounge.backend.model.request.CreatePostRequest
@@ -9,6 +10,7 @@ import com.gamelounge.backend.model.request.UpdatePostRequest
 import com.gamelounge.backend.model.response.ResponseMessage
 import com.gamelounge.backend.service.PostService
 import com.gamelounge.backend.util.ConverterDTO
+import com.gamelounge.backend.util.ConverterDTO.convertBulkToPostDTO
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.UUID
@@ -32,7 +34,6 @@ class PostController(private val postService: PostService) {
     }
 
     @PutMapping("/{id}")
-
     fun updatePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long, @RequestBody updatedPost: UpdatePostRequest): ResponseEntity<PostDTO> {
         val post = postService.updatePost(sessionId, id, updatedPost)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -40,7 +41,6 @@ class PostController(private val postService: PostService) {
     }
 
     @DeleteMapping("/{id}")
-
     fun deletePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<ResponseMessage> {
         postService.deletePost(sessionId, id)
         return ResponseEntity.ok(ResponseMessage(message = "Post deleted successfully"))
@@ -48,14 +48,18 @@ class PostController(private val postService: PostService) {
 
     @GetMapping
     fun getAllPosts(): ResponseEntity<List<PostDTO>> {
-        val posts = postService.getAllPosts()
-        val postsDTO = ConverterDTO.convertBulkToPostDTO(posts)
-        return ResponseEntity.ok(postsDTO)
+        val postDTOs = convertBulkToPostDTO(postService.getAllPosts())
+        return ResponseEntity.ok(postDTOs)
+    }
+
+    @GetMapping("/recommended")
+    fun getRecommendedPosts(@CookieValue("SESSIONID") sessionId: UUID?): ResponseEntity<List<PostDTO>> {
+        val postDTOs = postService.getRecommendedPosts(sessionId)
+        return ResponseEntity.ok(postDTOs)
     }
 
     // implement upvote endpoint
     @PutMapping("/{id}/upvote")
-
     fun upvotePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<PostDTO> {
         val post = postService.upvotePost(sessionId, id)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -64,7 +68,6 @@ class PostController(private val postService: PostService) {
 
     // implement downvote endpoint
     @PutMapping("/{id}/downvote")
-
     fun downvotePost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long): ResponseEntity<PostDTO> {
         val post = postService.downvotePost(sessionId, id)
         val postDTO = ConverterDTO.convertToPostDTO(post)
@@ -73,7 +76,6 @@ class PostController(private val postService: PostService) {
 
     // implement get upvoted users endpoint
     @GetMapping("/{id}/upvoteUsers")
-
     fun getUpvotedUsers(@PathVariable id: Long): ResponseEntity<List<UserDTO>> {
         val upvotedUsersDTO = postService.getUpvotedUsers(id)
         return ResponseEntity.ok(upvotedUsersDTO)
@@ -81,14 +83,12 @@ class PostController(private val postService: PostService) {
 
     // implement get downvoted users endpoint
     @GetMapping("/{id}/downvoteUsers")
-
     fun getDownvotedUsers(@PathVariable id: Long): ResponseEntity<List<UserDTO>> {
         val downvotedUsersDTO = postService.getDownvotedUsers(id)
         return ResponseEntity.ok(downvotedUsersDTO)
     }
     // REPORT POST
     @PostMapping("/{id}/report")
-
     fun reportPost(@CookieValue("SESSIONID") sessionId: UUID, @PathVariable id: Long, @RequestBody reqBody: ReportRequest): ResponseEntity<ResponseMessage> {
         postService.reportPost(sessionId, id, reqBody)
         return ResponseEntity.ok(ResponseMessage(message = "Post reported successfully"))
