91410fe33b6f47ca91a41f826e508dce3ca5584c
Legervad <emre.turker@boun.edu.tr>
2023-12-22 17:20:20 +0300

    #567 add necessary derived queries to repositories

--- app/backend/src/main/kotlin/com/gamelounge/backend/repository/PostRepository.kt
+++ app/backend/src/main/kotlin/com/gamelounge/backend/repository/PostRepository.kt
@@ -1,4 +1,5 @@
 package com.gamelounge.backend.repository
+import com.gamelounge.backend.entity.Game
 import org.springframework.stereotype.Repository
 import com.gamelounge.backend.entity.Post
 import com.gamelounge.backend.entity.User
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface PostRepository : JpaRepository<Post, Long>{
 
     fun findByUser(user: User): List<Post>
-
+    fun findByRelatedGame(game: Game): List<Post>
 }
