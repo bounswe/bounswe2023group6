e9b5254ad31ee3f1c55de0424c3b0cdb42296a64
Ömer Talip Akalın <omertalib42@gmail.com>
2023-12-25 03:05:29 +0300

    Implement game filtering at game page

--- app/frontend/src/pages/GameForum/GameForum.js
+++ app/frontend/src/pages/GameForum/GameForum.js
@@ -1,126 +1,159 @@
-import React from 'react'
-import 'tailwindcss/tailwind.css'
-import Navbarx from '../../components/navbar/Navbar'
-import { useState, useEffect } from 'react'
-import { getAllGames } from '../../services/gameService'
-import CreateGame from './CreateGame'
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
-import ImageListItemBar from '@mui/material/ImageListItemBar'
+import React, { useState, useEffect } from 'react';
+import Navbarx from '../../components/navbar/Navbar';
+import { getAllGames } from '../../services/gameService';
+import CreateGame from './CreateGame';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import gamePlaceHolder from '../../assets/img/game_place_holder.jpg'
+import gamePlaceHolder from '../../assets/img/game_place_holder.jpg';
 
 const GameForum = () => {
-  const [games, setGames] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredGames, setFilteredGames] = useState([]);
+    const [games, setGames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedGenre, setSelectedGenre] = useState('');
+    const [selectedPlatform, setSelectedPlatform] = useState('');
+    const [selectedPlayerNumber, setSelectedPlayerNumber] = useState('');
+    const [selectedUniverseInfo, setSelectedUniverseInfo] = useState('');
+    const [selectedGameMechanics, setSelectedGameMechanics] = useState('');
 
-  useEffect(() => {
-      const fetchGames = async () => {
-        setIsLoading(true)
-        try {
-          const response = await getAllGames()
-          setGames(response.data)
-        } catch (error) {
-          console.error(error)
-        } finally {
-          setIsLoading(false)
-        }
-      }
+    const predefinedGenres = ['RGP', 'Strategy', 'Shooter', 'Sports', 'Fighting', 'MOBA', 'Action', 'Adventure', 'Simulation', 'Horror', 'Empty'];
+    const predefinedPlatforms = ['XBOX', 'Computer', 'PS', 'Onboard', 'Mobile', 'Empty'];
+    const predefinedPlayerNumber = ['Single', 'Teams', 'Multiplayer', 'MMO', 'Empty'];
+    const predefinedUniverseInfo = ['Medieval', 'Fantasy', 'SciFi', 'Cyberpunk', 'Historical', 'Contemporary', 'PostApocalyptic', 'AlternateReality', 'Empty'];
+    const predefinedGameMechanics = ['TurnBased', 'ChangeBased', 'RealTime', 'Empty'];
 
-      fetchGames()
-    }, [])
+    useEffect(() => {
+        const fetchGames = async () => {
+            setIsLoading(true);
+            try {
+                const response = await getAllGames();
+                setGames(response.data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchGames();
+    }, []);
 
-  const handleImageError = (event) => {
-      event.target.src = gamePlaceHolder;
+    const handleGenreChange = (event) => {
+        setSelectedGenre(event.target.value);
     };
 
-  const handleSearchInputChange = (event) => {
-    const query = event.target.value.toLowerCase();
-    setSearchQuery(query);
+    const handlePlatformChange = (event) => {
+        setSelectedPlatform(event.target.value);
+    };
 
-    if (query === '') {
-      setFilteredGames([]);
-    } else {
-      const filtered = games.filter((game) => game.title.toLowerCase().includes(query));
-      setFilteredGames(filtered);
+    const handlePlayerNumberChange = (event) => {
+        setSelectedPlayerNumber(event.target.value);
+    };
+
+    const handleUniverseInfoChange = (event) => {
+        setSelectedUniverseInfo(event.target.value);
+    };
+
+    const handleGameMechanicsChange = (event) => {
+        setSelectedGameMechanics(event.target.value);
+    };
+
+    const handleImageError = (event) => {
+        event.target.src = gamePlaceHolder;
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value.toLowerCase());
+    };
+
+    if (isLoading) {
+        return <div className='items-center justify-center flex'>Loading...</div>;
     }
-  };
 
-  if (isLoading) {
-    return <div className='items-center justify-center flex'>Loading...</div>
-  }
-
-  return (
-    <>
-      <Navbarx></Navbarx>
-      <div className='w-full flex flex-col'>
-        <div className='w-full flex justify-center p-4 bg-neutral-50 pb-20'>
-          <div className='w-3/4 flex flex-col'>
-            <h1 className='text-4xl font-bold text-neutral-700 text-center'>Games</h1>
-            <div className='w-full flex flex-row justify-between items-center'>
-            <CreateGame />
-            <input
-              type="text"
-              placeholder="Search games..."
-              value={searchQuery}
-              onChange={handleSearchInputChange}
-              className="p-2 border border-gray-300 rounded-md"
-            />
+    return (
+        <>
+            <Navbarx></Navbarx>
+            <div className='w-full flex flex-col'>
+                <div className='w-full flex justify-center p-4 bg-neutral-50 pb-20'>
+                    <div className='w-3/4 flex flex-col'>
+                        <h1 className='text-4xl font-bold text-neutral-700 text-center'>Games</h1>
+                        <CreateGame />
+                        <div className='w-full justify-center items-center space-x-6 space-y-6 mb-6'>
+                            <select value={selectedPlatform} onChange={handlePlatformChange} className="p-2 border border-gray-300 rounded-md">
+                                <option value="">All Platforms</option>
+                                {predefinedPlatforms.map((platform) => (
+                                    <option key={platform} value={platform}>{platform}</option>
+                                ))}
+                            </select>
+                            <select value={selectedPlayerNumber} onChange={handlePlayerNumberChange} className="p-2 border border-gray-300 rounded-md">
+                                <option value="">All Player Numbers</option>
+                                {predefinedPlayerNumber.map((playerNumber) => (
+                                    <option key={playerNumber} value={playerNumber}>{playerNumber}</option>
+                                ))}
+                            </select>
+                            <select value={selectedUniverseInfo} onChange={handleUniverseInfoChange} className="p-2 border border-gray-300 rounded-md">
+                                <option value="">All Universe Info</option>
+                                {predefinedUniverseInfo.map((universeInfo) => (
+                                    <option key={universeInfo} value={universeInfo}>{universeInfo}</option>
+                                ))}
+                            </select>
+                            <select value={selectedGameMechanics} onChange={handleGameMechanicsChange} className="p-2 border border-gray-300 rounded-md">
+                                <option value="">All Game Mechanics</option>
+                                {predefinedGameMechanics.map((gameMechanics) => (
+                                    <option key={gameMechanics} value={gameMechanics}>{gameMechanics}</option>
+                                ))}
+                            </select>
+                            <select value={selectedGenre} onChange={handleGenreChange} className="p-2 border border-gray-300 rounded-md">
+                                <option value="">All</option>
+                                {predefinedGenres.map((genre) => (
+                                    <option key={genre} value={genre}>{genre}</option>
+                                ))}
+                            </select>
+                            <input
+                                type="text"
+                                placeholder="Search games..."
+                                value={searchQuery}
+                                onChange={handleSearchInputChange}
+                                className="p-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <ImageList className='w-full' cols={4} gap={8}>
+                            {console.log(games)}
+                            {games
+                                .filter((game) => selectedGenre === '' || game.genres.includes(selectedGenre))
+                                .filter((game) => selectedPlatform === '' || game.platforms.includes(selectedPlatform))
+                                .filter((game) => selectedPlayerNumber === '' || game.playerNumber.includes(selectedPlayerNumber))
+                                .filter((game) => selectedUniverseInfo === '' || game.universe.includes(selectedUniverseInfo))
+                                .filter((game) => selectedGameMechanics === '' || game.mechanics.includes(selectedGameMechanics))
+                                .filter((game) => searchQuery === '' || game.title.toLowerCase().includes(searchQuery))
+                                .map((game) => (
+                                    <ImageListItem key={game.gameId}>
+                                        <img
+                                            src={game.gamePicture || gamePlaceHolder}
+                                            alt={game.title}
+                                            loading="lazy"
+                                            onError={handleImageError}
+                                        />
+                                        <ImageListItemBar
+                                            title={<a href={`/game/${game.gameId}`} className='no-underline link text-white'>{game.title}</a>}
+                                            actionIcon={
+                                                <a href={`/game/${game.gameId}`} className='no-underline link'>
+                                                    <IconButton sx={{ color: 'rgba(255, 255, 255, 0.9)' }}>
+                                                        <ArrowForwardIosIcon />
+                                                    </IconButton>
+                                                </a>
+                                            }
+                                        />
+                                    </ImageListItem>
+                                ))}
+                        </ImageList>
+                    </div>
+                </div>
             </div>
-            <div className='w-full flex flex-row pt-4'>
-              <ImageList className='w-full h-full' style={{ gridTemplateColumns: 'repeat(4, 1fr)' }}>
-                                          {searchQuery
-                                            ? filteredGames
-                                                .filter((game) => game.status === 'APPROVED')
-                                                .map((game) => (
-                                                  <ImageListItem key={game.gameID} className='m-1'>
-                                                    <img
-                                                    src={game.gamePicture}
-                                                    alt={game.title}
-                                                    loading="lazy"
-                                                    onError={handleImageError}
-                                                    />
-                                                    <ImageListItemBar
-                                                      title={<a href={`/games/${game.gameId}`} className='no-underline link'>{game.title}</a>}
-                                                      actionIcon={
-                                                        <IconButton sx={{ color: 'rgba(255, 255, 255, 0.9)' }}>
-                                                          <ArrowForwardIosIcon />
-                                                        </IconButton>
-                                                      }
-                                                    />
-                                                  </ImageListItem>
-                                                ))
-                                            : games
-                                                .filter((game) => game.status === 'APPROVED')
-                                                .map((game) => (
-                                                  <ImageListItem key={game.gameId} className='m-1'>
-                                                    <img src={game.gamePicture} alt={game.title} loading="lazy" onError={handleImageError}/>
-                                                    <ImageListItemBar
-                                                      title={<a href={`/game/${game.gameId}`} className='no-underline link text-white'>{game.title}</a>}
-                                                      actionIcon={
-                                                        <a href={`/game/${game.gameId}`} className='no-underline link'>
-                                                                                <IconButton sx={{ color: 'rgba(255, 255, 255, 0.9)' }}>
-                                                                                  <ArrowForwardIosIcon />
-                                                                                </IconButton>
-                                                                              </a>
-                                                      }
-                                                    />
-                                                  </ImageListItem>
-                                                ))}
-                                        </ImageList>
-            </div>
-          </div>
-        </div>
-        <div className='bg-black text-white text-center p-8'>
-          <p className='text-m'>@2023 Game Lounge, All rights reserved.</p>
-        </div>
-      </div>
-    </>
-  )
-}
+        </>
+    );
+};
 
-export default GameForum
-
+export default GameForum;
