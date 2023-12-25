6b5ba421fb2f7c7283a9156e1bd55f894986d06b
Beyzanur Bektan <beyzabektan@gmail.com>
2023-12-26 02:09:40 +0300

    fix follow/unfollow

--- app/frontend/src/components/CommentCard.js
+++ app/frontend/src/components/CommentCard.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import userlogo from '../user.jpg'
 import ReportIcon from '@mui/icons-material/Report'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteComment } from '../services/commentService'
 
 const CommentCard = ({ comment, onUpvote, onDownvote, currentUser }) => {
 	const isCurrentUserCreator = comment.creatorUser.username === currentUser.username
 
+	const handleDeleteComment = async (commentId) => {
+    		try {
+    			await deleteComment(commentId)
+    			window.location.reload()
+    		} catch (error) {
+    			console.error('Error deleting post:', error)
+    		}
+    	}
+
 	return (
-		<div key={comment.commentId} className='card compact bg-neutral-300 text-zinc-800 shadow-xl m-4 p-4'>
+		<div key={comment.commentId} className='card compact bg-neutral-200 text-zinc-800 shadow-xl m-4 p-4'>
 			<div className='flex-col'>
 				<div className='flex flex-row mb-2'>
 					<p className='text-zinc-700 m-2 mb-4 w-full'>{comment.content}</p>
-					{isCurrentUserCreator ? null : (
+					{isCurrentUserCreator ? <button className='text-black rounded mb-4' onClick={() => handleDeleteComment(comment.commentId)}>
+                            									<DeleteIcon sx={{ color: '#404040' }} />
+                                            						</button> : (
 						<button className='text-black rounded mb-4'>
 							<ReportIcon sx={{ color: '#404040' }} />
 						</button>
