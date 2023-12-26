8be536679a4d1e2a1eb4c1e59a138c8bf6972809
Mustafa Küçük <44123646+mustafakucuk-dev@users.noreply.github.com>
2023-12-26 06:35:57 +0300

    backend url change to nginx reverse proxy

--- app/frontend/src/services/AnnotationService.js
+++ app/frontend/src/services/AnnotationService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
 	// baseURL: process.env.REACT_APP_API_URL
-	baseURL: 'http://167.99.242.175:8081'
+	baseURL: 'https://game-lounge.com:9091'
 })
 
 axiosInstance.defaults.withCredentials = true
